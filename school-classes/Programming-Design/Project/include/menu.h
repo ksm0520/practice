ef2206f86ad0a37708a24f542d4a034d12d8c449
ksm0520 <kimseungmin@gmail.com>
2025-05-18 23:48:18 +0900

    feat:사용자 이름 입력 기능 및 메뉴 내부 초기화 구현

--- school-classes/Programming-Design/Project/include/menu.h
+++ school-classes/Programming-Design/Project/include/menu.h
@@ -2,9 +2,7 @@
 #define MENU_H
 
 void showMainMenu();
-void startMatrixIntro();
-void drawBackgroundPattern(int width, int height);
-void printLogo();
-
+void gotoxy(int x, int y);         // 커서 위치 이동 함수
+extern char username[50];          // 사용자 이름 (전역 공유용)
 
 #endif
