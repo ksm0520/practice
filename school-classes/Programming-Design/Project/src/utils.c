f552fcc65e5686f8122f0a1066d088b76af21586
ksm0520 <kimseungmin@gmail.com>
2025-06-02 15:15:29 +0900

    fix:창모드 전용 배경 출력 범위 설정

--- school-classes/Programming-Design/Project/src/utils.c
+++ school-classes/Programming-Design/Project/src/utils.c
@@ -1,6 +1,17 @@
+#include <windows.h>
 #include <stdio.h>
 #include "utils.h"
 
 void clearBuffer() {
     while (getchar() != '\n');
+}
+
+void enforceExactConsoleSize() {
+    system("mode con: cols=146 lines=41");
+
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // 콘솔 창 크기 조절
+    SMALL_RECT rect = { 0, 0, 145, 40 }; // width-1, height-1
+    SetConsoleWindowInfo(hOut, TRUE, &rect);
 }
